2f59864421ef9ee8994a183d82da2b9fb763cdef
shizzz88 <shahzadciit@gmail.com>
2014-03-14 15:59:43 +0100

    First commit.

---
+++ src/LetusC_practice.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : LetusC_practice.c
+ Author      : Shahzad
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>	//file inclusion directive, locates file only in the specified directory!
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
